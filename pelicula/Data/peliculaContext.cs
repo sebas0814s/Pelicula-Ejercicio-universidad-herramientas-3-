81a05fe9951a1571963dc86d6b83f7d11b993eee
USER <USER@SEBAS>
2023-04-26 23:24:38 -0500

    Agregar archivos de proyecto.

---
+++ pelicula/Data/peliculaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using pelicula.Models;
+
+namespace pelicula.Data
+{
+    public class peliculaContext : DbContext
+    {
+        public peliculaContext (DbContextOptions<peliculaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<pelicula.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ pelicula/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pelicula.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
